41d5fe51f7d490fd2bb090977ca2312f60a7f916
SE-Abdelrahman <abdelrahman.k.gaber@gmail.com>
2026-02-24 01:25:53 +0200

    fo commit

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,6 +3,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'blog',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'blog/:slug',
+    renderMode: RenderMode.Client,
+  },
 ];
